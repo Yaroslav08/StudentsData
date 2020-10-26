e4e686701c084eee5dbea5261d120d14a0dad691
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-26 19:07:43 +0200

    Added some methods in IGroupService

--- StudentsData/StudentsData.Application/Interfaces/IGroupService.cs
+++ StudentsData/StudentsData.Application/Interfaces/IGroupService.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using StudentsData.Application.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+
 namespace StudentsData.Application.Interfaces
 {
     public interface IGroupService
     {
-
+        Task<string> CreateGroup(GroupCreateViewModel model);
+        Task<string> EditGroup(GroupEditViewModel model);
+        Task<string> RemoveGroup(int Id);
+        Task<GroupViewModel> GetGroupById(int Id);
+        Task<GroupViewModel> GetGroupByIdWithStudents(int Id);
+        Task<List<GroupViewModel>> GetAllGroups();
+        Task<List<GroupViewModel>> SearchGroups(string name);
     }
 }
