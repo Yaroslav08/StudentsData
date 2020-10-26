ce76ca2b54a92c026e02cb84cd1657a34341430c
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-26 18:39:05 +0200

    Added IUnitOfWork

---
+++ StudentsData/StudentsData.Infrastructure.Data/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using StudentsData.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace StudentsData.Infrastructure.Data
+{
+    public interface IUnitOfWork
+    {
+        ITeacherRepository Teachers { get; }
+        IGroupRepository Groups { get; }
+        IStudentRepository Students { get; }
+    }
+}
---
+++ StudentsData/StudentsData.Infrastructure.Data/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using StudentsData.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace StudentsData.Infrastructure.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        ITeacherRepository teacherRepos;
+
+        IGroupRepository groupRepos;
+
+        IStudentRepository studentRepos;
+
+        public UnitOfWork(ITeacherRepository _teacherRepos, IGroupRepository _groupRepos, IStudentRepository _studentRepos)
+        {
+            teacherRepos = _teacherRepos;
+            groupRepos = _groupRepos;
+            studentRepos = _studentRepos;
+        }
+
+        public ITeacherRepository Teachers => teacherRepos;
+
+        public IGroupRepository Groups => groupRepos;
+
+        public IStudentRepository Students => studentRepos;
+    }
+}
