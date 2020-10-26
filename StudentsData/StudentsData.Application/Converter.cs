d252973f876f96cd5009f4c25ce2613a190f8165
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-26 18:53:46 +0200

    Add Mapper Converter

---
+++ StudentsData/StudentsData.Application/Converter.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using StudentsData.Application.ViewModels;
+//using StudentsData.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace StudentsData.Application
+{
+    public class Converter : Profile
+    {
+        public Converter()
+        {
+            //CreateMap<Teacher, TeacherViewModel>();
+            //CreateMap<Group, GroupViewModel>();
+            //CreateMap<Student, StudentViewModel>();
+        }
+    }
+}
