7345522dc18e9b4f2493e855d73b162094e4e847
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-26 18:45:51 +0200

    Added IStudentService and ITeacherService

---
+++ StudentsData/StudentsData.Application/Services/StudentService.cs
@@ -0,0 +1,11 @@
+﻿using StudentsData.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace StudentsData.Application.Services
+{
+    public class StudentService : IStudentService
+    {
+
+    }
+}
