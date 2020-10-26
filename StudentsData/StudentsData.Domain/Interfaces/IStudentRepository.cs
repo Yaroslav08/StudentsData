1bf2a4e457d821926609744ca31d11643560f3b5
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-26 18:03:03 +0200

    Added some interfaces

---
+++ StudentsData/StudentsData.Domain/Interfaces/IStudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using StudentsData.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace StudentsData.Domain.Interfaces
+{
+    public interface IStudentRepository : IRepository<Student>
+    {
+
+    }
+}
