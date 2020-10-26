f83e7fec991abe7928de9cedd76349e6930de61c
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-10-26 22:02:48 +0200

    Added some views for group

--- StudentsData/StudentsData.MVC/Controllers/GroupController.cs
+++ StudentsData/StudentsData.MVC/Controllers/GroupController.cs
@@ -3,11 +3,88 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StudentsData.Application.Interfaces;
+using StudentsData.Application.ViewModels.Create;
+using StudentsData.Application.ViewModels.Edit;
 
 namespace StudentsData.MVC.Controllers
 {
+    [Route("group")]
     public class GroupController : Controller
     {
+        private readonly IGroupService groupService;
+        private readonly ILogger<GroupController> _logger;
+
+        public GroupController(ILogger<GroupController> logger, IGroupService _groupService)
+        {
+            _logger = logger;
+            groupService = _groupService;
+        }
+
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(GroupCreateViewModel model)
+        {
+            var res = await groupService.CreateGroup(model);
+            if (res != "OK")
+            {
+                ViewBag.Error = res;
+                return View("Error");
+            }
+            return LocalRedirect("~/group/all");
+        }
+
+        [HttpGet("edit/{Id}")]
+        public async Task<IActionResult> Edit(int Id)
+        {
+            var group = await groupService.GetGroupById(Id);
+            if (group == null)
+            {
+                ViewBag.Error = "Group not found";
+                return View("Error");
+            }
+            return View(new GroupEditViewModel
+            {
+                Id = group.Id,
+                Name = group.Name
+            });
+        }
+
+        [HttpPost("edit")]
+        public async Task<IActionResult> Edit(GroupEditViewModel model)
+        {
+            var res = await groupService.EditGroup(model);
+            if(res!="OK")
+            {
+                ViewBag.Error = res;
+                return View("Error");
+            }
+            return LocalRedirect("~/group/all");
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllGroups()
+        {
+            return View(await groupService.GetAllGroups());
+        }
+
+        [HttpGet("remove/{Id}")]
+        public async Task<IActionResult> RemoveGroup(int Id)
+        {
+            var res = await groupService.RemoveGroup(Id);
+            if (res != "OK")
+            {
+                ViewBag.Error = res;
+                return View("Error");
+            }
+            return LocalRedirect("~/group/all");
+        }
 
     }
 }
--- StudentsData/StudentsData.MVC/Controllers/StudentController.cs
+++ StudentsData/StudentsData.MVC/Controllers/StudentController.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StudentsData.Application.Interfaces;
 
 namespace StudentsData.MVC.Controllers
 {
+    [Route("student")]
     public class StudentController : Controller
     {
+        private readonly IStudentService studentService;
+        private readonly ILogger<StudentController> _logger;
+
+        public StudentController(ILogger<StudentController> logger, IStudentService _studentService)
+        {
+            _logger = logger;
+            studentService = _studentService;
+        }
+
+
 
     }
 }
