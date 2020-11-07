f793cd9300a145fee5e11f84c597fa879b8859b7
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-11-07 14:10:45 +0200

    Added project for telegram bot

--- StudentsData/StudentsData.MVC/Program.cs
+++ StudentsData/StudentsData.MVC/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Telegram.Bot;
+using Telegram.Bot.Args;
 
 namespace StudentsData.MVC
 {
@@ -13,9 +15,8 @@ namespace StudentsData.MVC
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();;
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
---
+++ StudentsData/TelegramBot/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var botClient = new TelegramBotClient("1312053961:AAHSoJatxCGgjFXOL-7DfWcNRDqq5HF6T4M");
+            var me = await botClient.GetMeAsync();
+            Console.WriteLine(
+              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+            );
+            botClient.OnMessage += async (sender, e) =>
+                 {
+                     if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+                     {
+                         switch (e.Message.Text)
+                         {
+                             case "/start":
+                                 {
+                                     await botClient.SendTextMessageAsync(e.Message.Chat, $"Наразі я підтримую лише дві команди: /hello та /rnd"); break;
+                                 }
+                             case "/hello":
+                                 {
+                                     await botClient.SendTextMessageAsync(e.Message.Chat.Id, $"Вітаю, {e.Message.From.FirstName}"); break;
+                                 }
+                             case "/rnd":
+                                 {
+                                     await botClient.SendTextMessageAsync(e.Message.Chat.Id, $"{new Random().Next(0, 100)}"); break;
+                                 }
+                             default:
+                                 break;
+                         }
+                     }
+                 };
+            botClient.StartReceiving();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            botClient.StopReceiving();
+        }
+
+        //static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        //{
+        //    if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+        //    {
+        //        switch (e.Message.Text)
+        //        {
+        //            case "/start":
+        //                {
+        //                    await botClient.SendTextMessageAsync(e.Message.Chat, $"Наразі я підтримую лише дві команди: /hello та /rnd"); break;
+        //                }
+        //            case "/hello":
+        //                {
+        //                    await botClient.SendTextMessageAsync(e.Message.Chat.Id, $"Вітаю, {e.Message.From.FirstName}"); break;
+        //                }
+        //            case "/rnd":
+        //                {
+        //                    await botClient.SendTextMessageAsync(e.Message.Chat.Id, $"{new Random().Next(0, 10)}"); break;
+        //                }
+        //            default:
+        //                break;
+        //        }
+        //    }
+        //}
+    }
+}
